9a690ac9aacfca653d251f6fe03741f3087b2404
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-01 11:03:03 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,38 +1,19 @@
 from bot import Bot
-
 from pyrogram.types import Message
-
 from pyrogram import filters
-
 from config import BOT_STATS_TEXT, USER_REPLY_TEXT
-
 from datetime import datetime
-
 from helper_func import get_readable_time
-
 # Define the authorized user ID
-
 AUTHORIZED_USER_ID = 123456789
-
 @Bot.on_message(filters.command('stats') & filters.user(AUTHORIZED_USER_ID))
-
 async def stats(bot: Bot, message: Message):
-
     now = datetime.now()
-
     delta = now - bot.uptime
-
     time = get_readable_time(delta.seconds)
-
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
-
 @Bot.on_message(filters.private & filters.incoming)
-
 async def useless(_, message: Message):
-
     if message.from_user.id == AUTHORIZED_USER_ID:
-
         if USER_REPLY_TEXT:
-
             await message.reply(USER_REPLY_TEXT)
-
